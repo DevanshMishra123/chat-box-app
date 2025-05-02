42e6d986189e289e8803eb1f3e1b35cc0e5bfd4b
DevanshMishra123 <mishra24devansh@gmail.com>
2025-05-03 01:24:54 +0530

    changed client side

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { io } from "socket.io-client";
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
